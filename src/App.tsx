3922421b4e9c496d6e1b56fbb9dc2bfdf28390a6
Andy Chou <acchou2@gmail.com>
2017-09-28 14:24:36 -0700

    Add `router5` and `rxjs-router5` deps. Add some prototype typings. Add basic calls to router API.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 import * as React from "react";
 import "./App.css";
+import Router5, { errorCodes, transitionPath, loggerPlugin, createRouter } from "router5";
+
+import browserPlugin from "router5/plugins/browser";
+import listenersPlugin from "router5/plugins/listeners";
+
+import * as Rx from "rxjs";
+
+import createObservables from "rxjs-router5";
+
+const routes = [{ name: "home", path: "/home" }, { name: "about", path: "/about" }];
+
+const { route$, routeNode, transitionError$, transitionRoute$ } = createObservables(
+    createRouter(routes)
+);
 
 const logo = require("./logo.svg");
 
---
+++ src/rxjs-router5.d.ts
@@ -0,0 +1,19 @@
+declare module "rxjs-router5" {
+    import * as Rx from "rxjs";
+    import { State, Router } from "router5";
+
+    interface RouterObservables {
+        // an observable of your application route
+        route$: Rx.Observable<State>;
+
+        // a function returning an observable of route updates for the specified node.
+        routeNode: (nodeName: string) => Rx.Observable<State>;
+
+        // an observable of transition errors
+        transitionError$: Rx.Observable<any>;
+
+        // an observable of the currently transitioning route
+        transitionRoute$: Rx.Observable<State>;
+    }
+    export default function createObservables(router: Router): RouterObservables;
+}
