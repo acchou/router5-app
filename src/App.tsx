e19ca4c7754fd433d03ee258b5d2dd5f7bc565ad
Andy Chou <acchou2@gmail.com>
2017-09-29 16:14:00 -0700

    Add observable debug widget, initial version

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,31 @@ const router = createRouter(routes);
 const { route$, routeNode, transitionError$, transitionRoute$ } = createObservables(router);
 
 const logo = require("./logo.svg");
+const eye = require("./eye.svg");
+
+interface ObserveProps {
+    // tslint:disable-next-line:no-any
+    [name: string]: Rx.Observable<any>;
+}
+
+// A debugging component to show observables that are driving the view
+function DebugObservablesWidget(props: ObserveProps) {
+    const widget = (
+        <img src={eye} className="observe-widget" onClick={() => console.log("clicked widget")} />
+    );
+    const results = Object.keys(props).map(key => (
+        <div className="observe-var">
+            <div className="observe-key">{key}</div>
+            <div className="observe-observable">{props[key]}</div>
+        </div>
+    ));
+    return (
+        <div className="observe-container">
+            {widget}
+            <div className="observe-panel">{results}</div>
+        </div>
+    );
+}
 
 interface BasicInputProps {
     onSubmit: (value: string | null) => void;
@@ -32,9 +57,16 @@ interface BasicInputProps {
 
 function BasicInput(props: BasicInputProps) {
     let value: string | null;
+
     return (
         <div className="basic-input">
-            <input type="text" onChange={newValue => (value = newValue.target.textContent)} />
+            <input
+                type="text"
+                onChange={newValue => {
+                    value = newValue.target.textContent;
+                    props.onSubmit(value);
+                }}
+            />
             <input type="button" onClick={() => props.onSubmit(value)} value="submit" />
         </div>
     );
@@ -74,6 +106,7 @@ const App = Recompose.componentFromStream(props$ => {
             <div className="App-header">
                 <h2>Welcome to React</h2>
             </div>
+            <DebugObservablesWidget />
             <div className="form">
                 <BasicInput onSubmit={val => submit(val ? val : "")} />
                 <span className="response">input: {input}</span>
